d7b354641ee0c79a78713809f1e234ddd50884fc
https://github.com/svkzdenosvk <zdenko.snurik@gmail.com>
2025-11-07 21:22:59 +0100

    fixing env path -> deleted slash

--- src/_inc/functions/register_related.tsx
+++ src/_inc/functions/register_related.tsx
@@ -45,7 +45,7 @@ export const handleRegister = async ({
   setError("");
 
   try {
-    const res = await fetch(`${import.meta.env.VITE_API_URL}/api/register`, {
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/register`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       credentials: "include",
