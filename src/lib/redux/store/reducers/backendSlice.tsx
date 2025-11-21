5398ad0dbe3707c0d8256b4c995ee3efe281e895
https://github.com/svkzdenosvk <zdenko.snurik@gmail.com>
2025-11-21 11:18:01 +0100

    add api behind / to path nest in redux

--- src/lib/redux/store/reducers/backendSlice.tsx
+++ src/lib/redux/store/reducers/backendSlice.tsx
@@ -38,7 +38,7 @@ const initialState: BackendState = {
 
 export const backendUrls: Record<My_Type_BE, string> = {
   express: "https://pexeso-2025-part-1-of-2-express-be.onrender.com/api",
-  nest: "https://pexeso2025part1of2nestbe-production.up.railway.app/",
+  nest: "https://pexeso2025part1of2nestbe-production.up.railway.app/api",
 };
 //https://pexeso-2025-part-1-of-2-nest-be.vercel.app/api
 
